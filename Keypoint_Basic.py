6594074fcad0dc928106b7c955de0b54d2343f96
bavensky <apiruk_sang-ngenchai@hotmai.com>
2024-08-22 13:05:54 +0900

    add basic code for yolo pose

--- Keypoint_Basic.py
+++ Keypoint_Basic.py
@@ -7,10 +7,10 @@ model = YOLO('yolov8n-pose.pt')
 def handle_request(req):
 	payload, img = getReqData(req)
 
-	results = model.predict(img, save=False, conf=0.5)[0]
+	results = model.predict(img, save=False, conf=0.5)[0] # only one person will be detected
 	
 	for result in results:
-		keypoints = result.keypoints  #
+		keypoints = result.keypoints  
 		print(keypoints)
 		
 	
