ae14a115e6442f006193ad6e86ee013500630d5a
bavensky <apiruk_sang-ngenchai@hotmai.com>
2024-08-22 13:05:28 +0900

    add basic code for yolo pose

---
+++ Keypoint_Basic.py
@@ -0,0 +1,22 @@
+#preparation here
+from ultralytics import YOLO
+
+
+model = YOLO('yolov8n-pose.pt') 
+
+def handle_request(req):
+	payload, img = getReqData(req)
+
+	results = model.predict(img, save=False, conf=0.5)[0]
+	
+	for result in results:
+		keypoints = result.keypoints  #
+		print(keypoints)
+		
+	
+	img = result.plot()
+	
+	resp = makeRespData(payload, img)
+	
+	return resp
+
